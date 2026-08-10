e731e4d13bb425ba9e42c000607c6e94a90b95cf
Astro Nomical <astro@dex602.com>
2026-08-10 19:46:05 +0000

    Exercise 2-5 Completed

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -11,4 +11,8 @@ export default class StudentTile extends LightningElement {
 	get tileSelected() {
 		return this.isSelected ? "tile selected" : "tile";
 	}
+
+	handleStudentClick() {
+		alert(this.student.Name);
+	}
 }
