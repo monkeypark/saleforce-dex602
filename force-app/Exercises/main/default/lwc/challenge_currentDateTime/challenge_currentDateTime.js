07f3180172dfe1b8bfdf17fbb9718e17b060e93b
Astro Nomical <astro@dex602.com>
2026-08-11 12:42:13 +0000

    challenge complete

---
+++ force-app/Exercises/main/default/lwc/challenge_currentDateTime/challenge_currentDateTime.js
@@ -0,0 +1,28 @@
+import { LightningElement } from "lwc";
+
+export default class Challenge_currentDateTime extends LightningElement {
+	currentDate = new Date();
+	handleUpdateDate() {
+		this.currentDate = new Date();
+	}
+
+	connectedCallback() {
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		setInterval(() => {
+			this.currentDate = new Date();
+		}, 1000);
+	}
+
+	/* 
+	
+	ES6 and 'this' TRIVIA: 
+	Why doesn't the following work?
+
+	connectedCallback() {
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		setInterval( function() {
+			this.currentDate = new Date();
+		}, 1000);
+	}
+	*/
+}
