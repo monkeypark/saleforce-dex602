9eaf68932efc3b31e85aa9e1f9d5c507b4967e30
Astro Nomical <astro@dex602.com>
2026-08-11 19:10:30 +0000

    Exercise 2-8 Completed

--- force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
+++ force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
@@ -1,3 +1,49 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
+import getInstructors from "@salesforce/apex/StudentBrowserForm.getInstructors";
+import getDeliveriesByInstructor from "@salesforce/apex/StudentBrowserForm.getDeliveriesByInstructor";
 
-export default class StudentBrowserForm extends LightningElement {}
+export default class StudentBrowserForm extends LightningElement {
+	instructors = [];
+	error;
+	selectedInstructorId = "";
+	deliveries = [];
+	selectedDeliveryId = "";
+
+	@wire(getInstructors)
+	wired_getInstructors({ error, data }) {
+		if (data) {
+			const options = data.map((instructor) => ({
+				value: instructor.Id,
+				label: instructor.Name
+			}));
+
+			this.instructors = [{ value: "", label: "Select an instructor" }, ...options];
+			this.error = undefined;
+		} else if (error) {
+			this.error = error;
+			this.instructors = [];
+		}
+	}
+
+	@wire(getDeliveriesByInstructor, { instructorId: "$selectedInstructorId" })
+	wired_getDeliveriesByInstructor({ error, data }) {
+		if (data && data.length) {
+			this.deliveries = data.map((delivery) => ({
+				value: delivery.Id,
+				label: `${delivery.Start_Date__c} ${delivery.Location__c} ${delivery.Attendee_Count__c} students`
+			}));
+
+			this.deliveries.unshift({
+				value: "",
+				label: "Any Delivery"
+			});
+		} else if (error) {
+			this.error = error;
+		}
+	}
+
+	handleInstructorChange(event) {
+		this.selectedInstructorId = event.target.value;
+		this.selectedDeliveryId = "";
+	}
+}
