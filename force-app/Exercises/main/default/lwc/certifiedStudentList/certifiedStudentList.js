a54aed97edb755b1ae361051f02d65b021841704
Ming <yiming.mp@gmail.com>
2026-08-13 13:11:05 +0000

    Exercise 4-4 Completed

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, wire } from "lwc";
 import getCertifiedStudents from "@salesforce/apex/CertifiedStudentList.getCertifiedStudents";
+import deleteStudentCertification from "@salesforce/apex/CertifiedStudentList.deleteStudentCertification";
+import { refreshApex } from "@salesforce/apex";
 
 export default class CertifiedStudentList extends LightningElement {
 	@api certificationId = 0;
@@ -7,6 +9,7 @@ export default class CertifiedStudentList extends LightningElement {
 	certifiedStudents;
 	error;
 	btnGroupDisabled = true;
+	_wiredStudentResult;
 
 	columnConfig = [
 		{ label: "Name", fieldName: "name", type: "text" },
@@ -17,6 +20,7 @@ export default class CertifiedStudentList extends LightningElement {
 
 	@wire(getCertifiedStudents, { certificationId: "$certificationId" })
 	wire_getCertifiedStudents(result) {
+		this._wiredStudentResult = result;
 		this.certifiedStudents = [];
 		if (result.data) {
 			this.certifiedStudents = result.data.map((certHeld) => ({
@@ -31,6 +35,39 @@ export default class CertifiedStudentList extends LightningElement {
 			this.error = result.error;
 		}
 	}
+
+	getSelectedIDs() {
+		const datatable = this.template.querySelector("lightning-datatable");
+		const ids = datatable.getSelectedRows().map((r) => r.certificationHeldId);
+		return ids;
+	}
+
+	handleCertActions(event) {
+		const btnClicked = event.target.getAttribute("data-btn-id");
+		switch (btnClicked) {
+			case "btnEmail":
+				break;
+			case "btnSendCert":
+				break;
+			case "btnDelete":
+				this.handleDelete();
+				break;
+			default:
+				break;
+		}
+	}
+
+	handleDelete() {
+		const certificationHeldIds = this.getSelectedIDs();
+		deleteStudentCertification({ certificationHeldIds })
+			.then(() => {
+				refreshApex(this._wiredStudentResult);
+			})
+			.catch((error) => {
+				this.error = error;
+			});
+	}
+
 	handleRowSelection(event) {
 		const numSelected = event.detail.selectedRows.length;
 		this.btnGroupDisabled = numSelected === 0;
